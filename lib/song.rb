e862a73a1eaf706a77646a0b2ce7a7506f7970cb
Jack Quinoa <jquinoa208@gmail.com>
2020-05-28 18:45:37 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,4 @@
+require "pry"
 class Song
   attr_accessor :name, :artist_name
   @@all = []
@@ -9,5 +10,62 @@ class Song
   def save
     self.class.all << self
   end
-
+  
+  def self.create
+    song = self.new 
+    @@all << song
+    song 
+  end
+  
+  def self.new_by_name(string_name)
+    song = self.new 
+    song.name = string_name
+    song 
+  end
+    
+  def self.create_by_name(string_name)
+    song = self.new 
+    song.name = string_name
+    @@all << song
+    song
+  end
+  
+  def self.find_by_name(string_name)
+    @@all.find{|song|  song.name == string_name}
+  end
+  
+  def self.find_or_create_by_name(string_name)
+    if self.find_by_name(string_name) 
+      return self.find_by_name(string_name)
+    else
+      self.create_by_name(string_name)
+    end
+  end
+  
+  def self.alphabetical
+    @@all.sort_by { |name| name.name }
+  end
+  
+  def self.new_from_filename(string)
+    binding.pry 
+    song = self.new
+    @@all << song 
+    song_array = []
+    song_array << string.split(/"-."/)
+    song_array.pop 
+    song.artist_name = song_array[0]
+    song.name = song_array[1]  
+    
+  end
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 end
